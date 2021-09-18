a9e0849947521cff2cb087d9a28bb5d498322fc1
yimin12 <hymlaucs@gmail.com>
2021-09-18 11:41:02 +0630

    second commit

---
+++ provision.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo 'Start!'
+
+# superuser名字
+USER="admin"
+# superuser密码
+PASS="admin"
+# superuser邮箱
+MAIL="admin@yitter.com"
+script="
+from django.contrib.auth.models import User;
+
+username = '$USER';
+password = '$PASS';
+email = '$MAIL';
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
+
+
+# 如果想直接进入/vagrant路径下
+# 请输入vagrant ssh命令进入
+# 手动输入
+# 输入ls -a
+# 输入 vi .bashrc
+# 在最下面，添加cd /vagrant
+
+echo 'All Done!'
